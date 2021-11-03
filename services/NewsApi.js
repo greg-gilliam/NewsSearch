a85c1648a6289e9f596afb86a382d2657bb681c8
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-03 15:38:40 -0700

    updated API link

--- services/NewsApi.js
+++ services/NewsApi.js
@@ -1,6 +1,6 @@
 export const fetchNews = async () => {
   const res = await fetch(
-    'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=75c0129a4ab14641b231784fca1186a2'
+    `https://newsapi.org/v2/top-headlines?country=us&category=business&${process.env.API_KEY}`
   );
   const news = await res.json();
   return news.results;
